9b8a1c33ada9d0d1093374981e6aad51a922c5a3
Raiosilva <raimundopi025@gmail.com>
2019-10-04 18:08:47 -0300

    adding controllers channels, talks, team_users, teams

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,51 @@
+class TeamsController < ApplicationController
+  before_action :set_team, only: [:destroy]
+  before_action :set_by_slug_team, only: [:show]
+
+  def index
+    @teams = current_user.teams
+  end
+
+  def show
+    # authorize => metodo cancancan para verificar
+    # se o current_user tem permmissão de leitura no record Team
+    authorize! :read, @team
+  end
+
+  def create
+    @team = Team.new(team_params)
+    # roda em tela/view
+    respond_to do |format|
+      if @team.save
+        format.html { redirect_to "/#{@team.slug}"  }
+      else
+        format.html { redirect_to main_app.root_url, notice: @team.errors }
+      end
+    end
+  end
+
+  def destroy
+    # verifica se é dono para delete
+    authorize! :destroy, @team
+    @team.destroy
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_by_slug_team
+    @team = Team.find_by(slug: params[:slug])
+  end
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:slug).merge(user: current_user)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'team_users/create'
+
+  get 'team_users/destroy'
+
+  get 'talks/show'
+
+  get 'channels/create'
+
+  get 'channels/destroy'
+
+  get 'channels/show'
+
+  get 'teams/index'
+
+  get 'teams/show'
+
+  get 'teams/create'
+
+  get 'teams/destroy'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
